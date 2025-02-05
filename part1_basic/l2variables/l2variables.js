fe9d36b0d5418128dbcc5da89b0ecc1d31339dbc
aungkyawzaww <aungkyawzaww7889@gmail.com>
2025-02-05 23:00:06 +0630

    5-2-25

---
+++ part1_basic/l2variables/l2variables.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+var luckynumber = 100;
+console.log(luckynumber);
+luckynumber = 200;
+console.log(luckynumber);
+// Run in terminal (Convert typescript to javascript)
+// tsc l2variables.ts
+// Run in terminal 
+// node l2variables.js
---
+++ part1_basic/l2variables/l2variables.ts
@@ -0,0 +1,16 @@
+console.log("Hello");
+
+let luckynumber:number = 100;
+console.log(luckynumber);
+
+luckynumber = 200;
+console.log(luckynumber);
+
+
+// Run in terminal (Convert typescript to javascript)
+// tsc l2variables.ts
+
+
+// Run in terminal 
+// node l2variables.js
+
